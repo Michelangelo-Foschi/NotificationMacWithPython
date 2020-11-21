a7420ae5c3c87024ee7662ca458dd840642407ee
Michelangelo Foschi <michelangelo.foschi.10@gmail.com>
2020-11-21 11:33:00 +0400

    Add code

--- code.py
+++ code.py
@@ -1,1 +1,12 @@
+import pync
 
+pync.notify('Hello World')
+pync.notify('Hello World', title='Python')
+pync.notify('Hello World', group=os.getpid())
+pync.notify('Hello World', activate='com.apple.Safari')
+pync.notify('Hello World', open='http://github.com/')
+pync.notify('Hello World', execute='Hey Man"')
+
+pync.remove_notifications(os.getpid())
+
+pync.list_notifications(os.getpid())
